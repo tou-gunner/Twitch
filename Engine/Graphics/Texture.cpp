d9c226d2fa1dfb2c5ac6c9279ea470a3ce175445
tou-gunner <91259667+tou-gunner@users.noreply.github.com>
2022-09-05 15:28:35 +0700

    Update 050920220

---
+++ Engine/Graphics/Texture.cpp
@@ -0,0 +1,53 @@
+#include "Texture.h"
+
+Texture::Texture() 
+{
+	id = -1;
+}
+
+Texture::Texture(int _id)
+{
+	id = _id;
+	if (!GetTextureParams())
+	{
+		std::cout << "Error loading image with ID: " << id << std::endl;
+	}
+}
+
+Texture::Texture(std::string path)
+{
+	id = SOIL_load_OGL_texture(path.c_str(), SOIL_LOAD_AUTO, SOIL_CREATE_NEW_ID, SOIL_FLAG_MULTIPLY_ALPHA);
+	if (!GetTextureParams())
+	{
+		std::cout << "Error loading image: " << path << std::endl;
+	}
+}
+
+int Texture::GetID()
+{
+	return id;
+}
+
+int Texture::GetWidth()
+{
+	return width;
+}
+
+int Texture::GetHeight()
+{
+	return height;
+}
+
+bool Texture::GetTextureParams()
+{
+	if (id > 0)
+	{
+		int miplevel = 0;
+		glBindTexture(GL_TEXTURE_2D, id);
+		glGetTexLevelParameteriv(GL_TEXTURE_2D, miplevel, GL_TEXTURE_WIDTH, &width);
+		glGetTexLevelParameteriv(GL_TEXTURE_2D, miplevel, GL_TEXTURE_HEIGHT, &height);
+		return true;
+	}
+
+	return false;
+}
---
+++ Engine/Graphics/Texture.h
@@ -0,0 +1,28 @@
+#ifndef TWITCH_TEXTURE
+#define TWITCH_TEXTURE
+
+#include <iostream>
+#include "GLFW/glfw3.h"
+#include "SOIL/src/SOIL.h"
+
+class Texture
+{
+public:
+	Texture();
+	Texture(int _id);
+	Texture(std::string path);
+
+	int GetID();
+	int GetWidth();
+	int GetHeight();
+
+private:
+	bool GetTextureParams();
+
+	int id;
+	int width;
+	int height;
+
+};
+
+#endif
